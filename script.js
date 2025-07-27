99d39a728eedc67ac40f6217c4d96285e87924d5
Syed Hastman Jan <hastamanjan@yahoo.com>
2025-07-28 02:01:38 +0500

    Practiced ecmascript/javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+//  Alert exercise practice
+
+//exercise 1: Using alert and storing the string data type in quotation marks 
+alert("Hello World");
+
+
+//exercise 2: Using alert and displaying name in place of Hello world
+alert("Syed Hastman Jan");
+
+//exercise 3: Using alert and storing the numirecal data type without quotation marks
+alert(10);
+
+
+//exercise 4: Using varible named studentName and storing the string data Syed Hastman Jan then displaying that variable in alert
+var studentName = "Syed Hastman Jan";
+alert(studentName);
+
+//exercise 5: Using varibles declaration let and combining 2 strings and displaying them in alert
+let greet = "Hi this is " + studentName;
+alert(greet);
+
